58a7985813a50ebc3632823ac1651ceac83dfd8f
vishnumishra <vishnu143mishra@gmail.com>
2015-03-15 21:30:40 +0530

    compare method can compare that a unit is equal to another unit

--- UnitsTest/UnitsTest.java
+++ UnitsTest/UnitsTest.java
@@ -8,70 +8,70 @@ import static org.junit.Assert.assertEquals;
 public class UnitsTest {
     @Test
     public void UnitShouldCompareBetweenTwoUnits_01(){
-        Units u1 = Units.create(Units.ListOfUnits.feet,1);
-        Units u2 = Units.create(Units.ListOfUnits.inch,12);
+        Units u1 = Units.create(Units.LengthUnits.feet,1);
+        Units u2 = Units.create(Units.LengthUnits.inch,12);
         assertEquals(u2.compare(u1),0);
     }
     @Test
     public void UnitShouldCompareBetweenTwoUnits_02(){
-        Units u1 = Units.create(Units.ListOfUnits.feet,1);
-        Units u2 = Units.create(Units.ListOfUnits.inch,12);
+        Units u1 = Units.create(Units.LengthUnits.feet,1);
+        Units u2 = Units.create(Units.LengthUnits.inch,12);
         assertEquals(u1.compare(u2),0);
     }
 
     @Test
     public void UnitShouldCompareBetweenFeetToCm_01(){
-        Units u1 = Units.create(Units.ListOfUnits.feet,1);
-        Units u2 = Units.create(Units.ListOfUnits.cm,12);
+        Units u1 = Units.create(Units.LengthUnits.feet,1);
+        Units u2 = Units.create(Units.LengthUnits.cm,12);
         assertEquals(u2.compare(u1),-7);
     }
     @Test
     public void UnitShouldCompareBetweenFeetToCm_02(){
-        Units u1 = Units.create(Units.ListOfUnits.feet,1);
-        Units u2 = Units.create(Units.ListOfUnits.cm,12);
+        Units u1 = Units.create(Units.LengthUnits.feet,1);
+        Units u2 = Units.create(Units.LengthUnits.cm,12);
         assertEquals(u1.compare(u2),7);
     }
     @Test
     public void UnitShouldCompareBetweenInchToCm_01(){
-        Units u1 = Units.create(Units.ListOfUnits.inch,2);
+        Units u1 = Units.create(Units.LengthUnits.inch,2);
         int expected = -(int) (2 - (0.8*0.4));
-        Units u2 = Units.create(Units.ListOfUnits.cm,0.8);
+        Units u2 = Units.create(Units.LengthUnits.cm,0.8);
         assertEquals(expected,u2.compare(u1));
     }
     @Test
     public void UnitShouldCompareBetweenInchToCm_02(){
-        Units u1 = Units.create(Units.ListOfUnits.inch,2);
+        Units u1 = Units.create(Units.LengthUnits.inch,2);
         int expected = (int) (2 - (0.8*0.4));
-        Units u2 = Units.create(Units.ListOfUnits.cm,0.8);
+        Units u2 = Units.create(Units.LengthUnits.cm,0.8);
         assertEquals(expected,u1.compare(u2));
     }
     @Test
     public void UnitShouldCompareBetweenInchToM_01(){
-        Units u1 = Units.create(Units.ListOfUnits.inch,1);
-        Units u2 = Units.create(Units.ListOfUnits.cm,0.4);
+        Units u1 = Units.create(Units.LengthUnits.inch,1);
+        Units u2 = Units.create(Units.LengthUnits.cm,0.4);
         assertEquals(0,u2.compare(u1));
     }
     @Test
     public void UnitShouldCompareBetweenInchToM_02(){
-        Units u1 = Units.create(Units.ListOfUnits.inch,1);
-        Units u2 = Units.create(Units.ListOfUnits.cm,0.4);
+        Units u1 = Units.create(Units.LengthUnits.inch,1);
+        Units u2 = Units.create(Units.LengthUnits.cm,0.4);
         assertEquals(0,u1.compare(u2));
     }
     @Test
     public void UnitShouldCompareBetweenInchToM_03(){
-        Units u1 = Units.create(Units.ListOfUnits.inch,1);
+        Units u1 = Units.create(Units.LengthUnits.inch,1);
         assertEquals(0,u1.compare(u1));
     }
     @Test
     public void UnitShouldCompareBetweenFeetToM_01(){
-        Units u1 = Units.create(Units.ListOfUnits.feet,1);
-        Units u2 = Units.create(Units.ListOfUnits.m,0.3);
+        Units u1 = Units.create(Units.LengthUnits.feet,1);
+        Units u2 = Units.create(Units.LengthUnits.m,0.3);
         assertEquals(0,u1.compare(u2));
     }
     @Test
     public void UnitShouldCompareBetweenFeetToM_02(){
-        Units u1 = Units.create(Units.ListOfUnits.feet,1);
-        Units u2 = Units.create(Units.ListOfUnits.m,0.3);
+        Units u1 = Units.create(Units.LengthUnits.feet,1);
+        Units u2 = Units.create(Units.LengthUnits.m,0.3);
         assertEquals(0,u2.compare(u1));
     }
 
--- src/Units.java
+++ src/Units.java
@@ -6,34 +6,34 @@ import java.util.Map;
  */
 public class Units {
 
-    enum ListOfUnits{inch,feet,cm,m} ListOfUnits unitList;
-    private Map<ListOfUnits, Double> toInch;
-    private ListOfUnits u;
+    enum LengthUnits{inch,feet,cm,m}
+
+    private Map<LengthUnits, Double> toInch;
+    private LengthUnits u;
     private double value;
 
-    private Units(ListOfUnits u,double value) {
+    private Units(LengthUnits u,double value) {
         this.u = u;
         this.value = value;
         this.toInch = mapUnitToInch();
     }
 
-    private Map<ListOfUnits, Double> mapUnitToInch() {
-        toInch = new HashMap<ListOfUnits, Double>();
-        toInch.put(ListOfUnits.feet,12.0);
-        toInch.put(ListOfUnits.inch,1.0);
-        toInch.put(ListOfUnits.m,40.0);
-        toInch.put(ListOfUnits.cm,0.4);
+    private Map<LengthUnits, Double> mapUnitToInch() {
+        toInch = new HashMap<LengthUnits, Double>();
+        toInch.put(LengthUnits.feet,12.0);
+        toInch.put(LengthUnits.inch,1.0);
+        toInch.put(LengthUnits.m,40.0);
+        toInch.put(LengthUnits.cm,0.4);
         return toInch;
     }
 
-    public static Units create(ListOfUnits u, double value){
+    public static Units create(LengthUnits u, double value){
         return new Units(u,value);
     }
 
     public int compare(Units unit) {
         double value1 = toInch.get(this.u)*this.value;
         double value2 = toInch.get(unit.u)*unit.value;
-        System.out.println(value1+" "+value2+" "+(value1 >value2)+" "+(value2 >value1));
         return (value1 >value2||value2 >value1)? (int) (value1 - value2) :0;
     }
 }
